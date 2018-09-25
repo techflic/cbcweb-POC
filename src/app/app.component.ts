1a5d1b9011ae04f0945a153a51e20984c5370e70
vijayyennam <vjay11yenam@gmail.com>
2018-09-25 13:49:39 +0530

    initialised

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>CBCWEB</h1>
+  `,
+  styles: [``]
 })
 export class AppComponent {
-  title = 'app';
+  
 }
